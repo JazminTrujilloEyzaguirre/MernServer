a92d8cf74785679e7487334b5951562dc4acdfea
Jazmin Jahaziel <jazmin.jahazieltru@gmail.com>
2021-04-05 12:09:56 -0400

    cambios para deployment1

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 // Rutas para autenticar usuarios
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 const authController = require('../controllers/authController');
 const auth = require('../middleware/auth');
 
-// Iniciar session
+// Iniciar sesión
 // api/auth
 router.post('/', 
     authController.autenticarUsuario
@@ -14,7 +15,6 @@ router.post('/',
 router.get('/',
     auth,
     authController.usuarioAutenticado
-
-);
+)
 
 module.exports = router;
